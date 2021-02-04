a2dd25dd518dd82fcc725148afe4fa4cc33d1527
rehanalam <m.rehan.alam@gmail.com>
2021-02-04 15:48:16 +0500

    fixes propType warnings

--- src/types.js
+++ src/types.js
@@ -1,12 +1,12 @@
 import PropTypes from "prop-types";
 
 export const registry = PropTypes.shape({
-  ArrayFieldTemplate: PropTypes.elementType,
-  FieldTemplate: PropTypes.elementType,
-  ObjectFieldTemplate: PropTypes.elementType,
+  ArrayFieldTemplate: PropTypes.func,
+  FieldTemplate: PropTypes.func,
+  ObjectFieldTemplate: PropTypes.func,
   definitions: PropTypes.object.isRequired,
   rootSchema: PropTypes.object,
-  fields: PropTypes.objectOf(PropTypes.elementType).isRequired,
+  fields: PropTypes.objectOf(PropTypes.func).isRequired,
   formContext: PropTypes.object.isRequired,
   widgets: PropTypes.objectOf(
     PropTypes.oneOfType([PropTypes.func, PropTypes.object])
