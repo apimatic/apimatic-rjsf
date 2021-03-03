a996f6289e9acd7f260c55b6ccd17e8f38ba6e20
WaqasJaved1 <jwaqas42@gmail.com>
2021-03-03 15:59:04 +0500

    Fixed React warnings for icon styling properties

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -44,9 +44,9 @@ export const DeleteIcon = ({ width, color }) => (
     stroke={color || DEFAULT_GRAY}
   >
     <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      stroke-width="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
       d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
     />
   </svg>
@@ -66,9 +66,9 @@ export const ArrowUpIcon = ({ width, color, rotate }) => (
     stroke={color || DEFAULT_GRAY}
   >
     <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      stroke-width="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
       d="M5 10l7-7m0 0l7 7m-7-7v18"
     />
   </svg>
@@ -85,9 +85,9 @@ export const ChevronIcon = ({ width, color, rotate }) => (
     stroke={color || DEFAULT_GRAY}
   >
     <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      stroke-width="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
       d="M19 9l-7 7-7-7"
     />
   </svg>
