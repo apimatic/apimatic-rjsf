0ccf3acd1e6e5b9d2ecfb71618d568db5d295da9
rehanalam <m.rehan.alam@gmail.com>
2021-11-26 09:59:10 +0500

    synced sample  ad updates webpack dev config

--- playground/samples/simple.js
+++ playground/samples/simple.js
@@ -221,60 +221,304 @@ module.exports = {
   formData: {}
 };
 
-// module.exports = {
-//   schema: {
-//     "title": "Endpoint Arguments",
-//     "type": "object",
-//     "additionalProperties": false,
-//     "properties": {
-//       "args": {
-//         "id": "endpoint_7C26ABBCDB1F9",
-//         "title": "endpoint_7C26ABBCDB1F9",
-//         "type": "object",
-//         "properties": {
-//           "Array":{
-//             "type": "array",
-//             "items": {
-//               "type": "number"
-//             }
-//           },
-//           "Simple oneOf Array": {
-//             "oneOf": [
-//               {
-//                 "type": "array",
-//                 "items": {
-//                   "type": "number"
-//                 }
-//               },
-//               {
-//                 "type": "array",
-//                 "items": {
-//                   "type": "boolean"
-//                 }
-//               }
-//             ]
-//           },
-//           // "Simple boolean array + number": {
-//           //   "oneOf": [{
-//           //       "type": "array",
-//           //       "items": {
-//           //         "type": "number"
-//           //       }
-//           //     },
-//           //     {
-//           //       "type": "boolean"
-//           //     }
-//           //   ]
-//           // },
-//         },
-//         "required": [
+module.exports = {
+  schema: {
+    title: "Endpoint Arguments",
+    type: "object",
+    additionalProperties: false,
+    properties: {
+      args: {
+        id: "endpoint_7C26ABBCDB1F9",
+        title: "endpoint_7C26ABBCDB1F9",
+        type: "object",
+        properties: {
+          // "Array":{
+          //   "type": "array",
+          //   "items": {
+          //     "type": "number"
+          //   }
+          // },
+          // "Simple oneOf Array": {
+          //   "oneOf": [
+          //     {
+          //       "type": "array",
+          //       "items": {
+          //         "type": "number"
+          //       }
+          //     },
+          //     {
+          //       "type": "array",
+          //       "items": {
+          //         "type": "boolean"
+          //       }
+          //     }
+          //   ]
+          // },
+          // "Simple boolean array + number": {
+          //   "oneOf": [{
+          //       "type": "array",
+          //       "items": {
+          //         "type": "number"
+          //       }
+          //     },
+          //     {
+          //       "type": "boolean"
+          //     }
+          //   ]
+          // },
+          // "send_oneof_inner_arrayOfMap_flag": {
+          //   "oneOf": [{
+          //       "type": "array",
+          //       "items": {
+          //         "type": "object",
+          //         "additionalProperties": {
+          //           "type": "number"
+          //         }
+          //       }
+          //     },
+          //     {
+          //       "type": "array",
+          //       "items": {
+          //         "type": "object",
+          //         "additionalProperties": {
+          //           "type": "boolean"
+          //         }
+          //       }
+          //     }
+          //   ]
+          // },
+          // "send_oneof_mapOfArray": {
+          //   "type": "object",
+          //   "additionalProperties": {
+          //     "oneOf": [
+          //       {
+          //         "type": "number"
+          //       },
+          //       {
+          //         "type": "boolean"
+          //       }
+          //     ]
+          //   }
+          // },
+          // "send_oneof_inner_mapOfArray": {
+          //   "oneOf": [
+          //     {
+          //       "type": "object",
+          //       "additionalProperties": {
+          //         "type": "array",
+          //         "items": {
+          //           "type": "number"
+          //         }
+          //       }
+          //     },
+          //     {
+          //       "type": "object",
+          //       "additionalProperties": {
+          //         "type": "array",
+          //         "items": {
+          //           "type": "boolean"
+          //         }
+          //       }
+          //     }
+          //   ]
+          // },
+          send_oneof_arrayOfMap_flag_of_mixed_arrayOfMap_flag: {
+            description: "",
+            type: "array",
+            items: {
+              type: "object",
+              additionalProperties: {
+                oneOf: [
+                  {
+                    type: "array",
+                    items: {
+                      type: "object",
+                      additionalProperties: {
+                        type: "number"
+                      }
+                    }
+                  },
+                  {
+                    type: "boolean"
+                  }
+                ]
+              }
+            }
+          }
+        },
 
-//         ]
-//       }
-//     },
-//     "required": [
-//       "args"
-//     ]
-//   },
-//   formData: {}
-// };
+        required: []
+      }
+    },
+    required: ["args"]
+  },
+  formData: {
+    args: {
+      send_oneof_arrayOfMap_flag_of_mixed_arrayOfMap_flag: [
+        {
+          key0: [
+            {
+              key0sadds: 12331321321
+            },
+            {
+              asdasdd: 32444224424
+            }
+          ],
+          key2: true
+        }
+      ]
+    }
+  }
+};
+
+module.exports = {
+  schema: {
+    value: {
+      oneOf: [
+        {
+          type: "number"
+        },
+        {
+          type: "boolean"
+        }
+      ]
+    }
+  },
+  formData: ""
+};
+
+module.exports = {
+  schema: {
+    title: "Endpoint Arguments",
+    type: "object",
+    additionalProperties: false,
+    properties: {
+      args: {
+        id: "endpoint_7C26ABBCDB1F9",
+        title: "endpoint_7C26ABBCDB1F9",
+        type: "object",
+        properties: {
+          isPrimitiveType: {
+            type: "boolean"
+          },
+          isAtEndpointLevel: {
+            type: "boolean"
+          },
+          hasDiscriminator: {
+            type: "boolean"
+          },
+          nestedObject: {
+            id: "nestedObject",
+            title: "nestedObject",
+            type: "object",
+            properties: {
+              nested_object_oneOf: {
+                oneOf: [
+                  {
+                    type: "object",
+                    additionalProperties: {
+                      type: "array",
+                      items: {
+                        type: "number"
+                      }
+                    }
+                  },
+                  {
+                    type: "object",
+                    additionalProperties: {
+                      type: "array",
+                      items: {
+                        type: "boolean"
+                      }
+                    }
+                  }
+                ]
+              }
+            }
+          },
+          send_oneof_inner_mapOfArray: {
+            oneOf: [
+              {
+                type: "object",
+                additionalProperties: {
+                  type: "array",
+                  items: {
+                    type: "number"
+                  }
+                }
+              },
+              {
+                type: "object",
+                additionalProperties: {
+                  type: "array",
+                  items: {
+                    type: "boolean"
+                  }
+                }
+              }
+            ]
+          },
+          session: {
+            description: "Course session",
+            oneOf: [
+              {
+                id: "Morning",
+                title: "Morning",
+                description: "Course morning session",
+                type: "object",
+                properties: {
+                  startsAt: {
+                    description: "Session start time",
+                    type: "string"
+                  },
+                  endsAt: {
+                    description: "Session end time",
+                    type: "string"
+                  },
+                  offerTeaBreak: {
+                    description: "Offer tea break during session",
+                    type: "boolean"
+                  }
+                },
+                required: ["startsAt", "endsAt", "offerTeaBreak"]
+              },
+              {
+                id: "Evening",
+                title: "Evening",
+                description: "Course evening session",
+                type: "object",
+                properties: {
+                  startsAt: {
+                    description: "Session start time",
+                    type: "string"
+                  },
+                  endsAt: {
+                    description: "Session end time",
+                    type: "string"
+                  },
+                  offerDinner: {
+                    description: "Offer dinner during session",
+                    type: "boolean"
+                  }
+                },
+                required: ["startsAt", "endsAt", "offerDinner", "offerTeaBreak"]
+              }
+            ]
+          }
+        },
+        required: [
+          "isPrimitiveType",
+          "isAtEndpointLevel",
+          "hasDiscriminator",
+          "session"
+        ]
+      }
+    },
+    required: ["args"]
+  },
+  formData: {
+    // session: {
+    //   offerDinner: true
+    // }
+  }
+};
