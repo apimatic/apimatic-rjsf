de75fa59be52e6e06135413fa3c70b28971e1de7
Inam-urRehman <mindgripper.developement@gmail.com>
2022-07-06 11:47:53 +0500

    validtion on optional fields

--- src/validationUtils.js
+++ src/validationUtils.js
@@ -14,25 +14,34 @@ function isPrimitiveSchema(schema) {
     schema.type !== "object" &&
     schema.type !== "array" &&
     !schema.hasOwnProperty("oneOf") &&
-    !schema.hasOwnProperty("oneOf")
+    !schema.hasOwnProperty("anyOf")
   );
 }
 
 export function validateField(schema, formData, required, disabled) {
   let errors = [];
+  // We shows validation only on enabled fields
+  if (!disabled) {
+    // if formData === undefined it means that we are in initial state
+    if (formData === undefined) {
+      // if optional field we don't need to show errors;
+      // otherwise we will show required error on initial state.
+      if (!required) {
+        return errors;
+      } else {
+        errors = ["is a required field"];
+      }
+    } else if (isPrimitiveSchema(schema)) {
+      const clonedSchema = deepClone(schema);
+      delete clonedSchema.id;
 
-  if (required && formData === undefined && !disabled) {
-    errors = ["is a required property"];
-  } else if (isPrimitiveSchema(schema) && !disabled) {
-    const clonedSchema = deepClone(schema);
-    delete clonedSchema.id;
+      const ajv = AJV.getInstance();
+      ajv.validate(clonedSchema, formData);
 
-    const ajv = AJV.getInstance();
-    ajv.validate(clonedSchema, formData);
-
-    errors = ajv.errors
-      ? [...ajv.errors.map(err => err.message), ...errors]
-      : errors;
+      errors = ajv.errors
+        ? [...ajv.errors.map(err => err.message), ...errors]
+        : errors;
+    }
   }
 
   return errors;
