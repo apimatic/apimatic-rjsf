9416c7d2fdb2ad9514d8ca19219114f3afbe19ae
mtalhafaheem <talha.faheem@apimatic.io>
2022-10-03 17:15:32 +0500

    Extra section in Discriminator Child

--- src/utils.js
+++ src/utils.js
@@ -519,7 +519,7 @@ export function optionsList(schema) {
   }
 }
 
-function findSchemaDefinition($ref, definitions = {}, fallback) {
+export function findSchemaDefinition($ref, definitions = {}, fallback) {
   // Extract and use the referenced definition if we have it.
   const match = /^ModelSchemas#\/(.*)$/.exec($ref);
   if (match && match[1]) {
